c25a6abd6de500f9c39354cca84e6029b6404641
kun-zjk <zhujunkun10@126.com>
2021-10-26 12:09:31 +0800

    zjk 1026

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 from .api.RecEngine import rec_engine
 from .utils import gen_uuid
+import os
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'F://nus_class//iss_project//robot//code//iss-project-agent-yvrh-8508c4e851cb.json'
 
+DIALOGFLOW_PROJECT_ID = 'iss-project-agent-yvrh'
+DIALOGFLOW_LANGUAGE_CODE = 'en'
+Session123 = {}
 # 通过 static_folder 指定静态资源路径，以便 index.html 能正确访问 CSS 等静态资源
 # template_folder 指定模板路径，以便 render_template 能正确渲染 index.html
 # static_url_path 指定访问的路径
@@ -10,6 +17,8 @@ app = Flask(
     static_folder="./static",
     static_url_path="/",
     template_folder="./static")
+
+
 
 @app.route('/')
 def index():
@@ -38,17 +47,39 @@ def sned_msg():
     # TODO: 校验请求信息，没有信息或者信息格式不对，返回错误码
 
     # TODO: 把用户的消息发送给 dialogflow 机器人，拿到机器人回复（这里可能涉及需要多个机器人实例）
+    # SESSION_ID = gen_uuid()
+    SESSION_ID = 'me'
+    if  SESSION_ID not in Session123:
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+        Session123[SESSION_ID]={}
+        Session123[SESSION_ID]["session"]=  session
+        Session123[SESSION_ID]["session_client"]=  session_client
+    else:
+        session=Session123[SESSION_ID]["session"]
+        session_client=Session123[SESSION_ID]["session_client"] 
 
+
+    text_to_be_analyzed = message
     
+    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
 
     # TODO: 根据返回数据判断是否需要推荐列表
 
-    need_recommend = True # 是否要推荐列表
-
+    need_recommend = False # 是否要推荐列表
+    
     # 如果不需要推荐，直接返回机器人的回复给用户
     if not need_recommend:
+        # response的结构
+        print ("this is response")
+        print (response)
         return_data = { # 返回给前端的数据
-            "response": f"This is normal {count_index} response"
+            "response": response.query_result.fulfillment_text
         }
         # TODO: return_data 按API文档，构造成机器人的回复格式
         
